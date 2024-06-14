6522aa1cda96aa510f02190dfe782d3dd82d4abd
Norie <norie.macewan@tiscali.co.uk>
2024-06-14 16:33:42 +0100

    Updated header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,9 @@
 import React from "react";
-
+import logo from '../assets/investment-calculator-logo.png'
 const Header = () => {
   return (
     <div id="header">
-      <img src="investment-calculator-logo.png" />
+      <img src={logo}/>
       <h1>React Investment Calculator</h1>
     </div>
   );
