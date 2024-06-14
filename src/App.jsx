62541b5b5a9a1d09202c4e5cf9e3a89611481f51
Norie <norie.macewan@tiscali.co.uk>
2024-06-14 16:27:50 +0100

    Inital commit, added header component

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Header from "./components/Header"
+
+function App() {
+  return (
+    <main>
+      <Header />
+      
+      
+
+    </main>
+  )
+}
+
+export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div id="header">
+      <img src="investment-calculator-logo.png" />
+      <h1>React Investment Calculator</h1>
+    </div>
+  );
+};
+
+export default Header;
